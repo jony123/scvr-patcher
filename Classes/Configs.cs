529f070f1b13207dd993ea31b7e4f7fc30468336
Jonathon Hill <hill.jonathon.paul@gmail.com>
2025-03-06 09:45:50 +1300

    Linting Fixes

--- Classes/Configs.cs
+++ Classes/Configs.cs
@@ -3,7 +3,8 @@
 #pragma warning disable CS8601
 #pragma warning disable CS8603
 
-namespace SCVRPatcher {
+namespace SCVRPatcher
+{
     using System;
     using System.Collections.Generic;
 
@@ -16,7 +17,8 @@ namespace SCVRPatcher {
     using Brand = Dictionary<string, Dictionary<string, Dictionary<string, HmdConfig>>>;
     using System.Net.Http.Json;
 
-    public partial class ConfigDataBase {
+    public partial class ConfigDataBase
+    {
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
         [JsonIgnore]
@@ -35,7 +37,8 @@ namespace SCVRPatcher {
         [JsonIgnore]
         public bool IsEmptyOrMissing => Brands is null || Brands.Count == 0;
 
-        public HmdConfig GetConfig(string brand, string hmd, string config) {
+        public HmdConfig GetConfig(string brand, string hmd, string config)
+        {
             Logger.Info($"Trying to get config for {brand}/{hmd}/{config}");
             var b = Brands.First(b => b.Key == brand);
             var h = b.Value.First(h => h.Key == hmd);
@@ -43,12 +46,17 @@ namespace SCVRPatcher {
             return c.Value;
         }
 
-        public List<string> GetPath(HmdConfig config) {
+        public List<string> GetPath(HmdConfig config)
+        {
             var path = new List<string>();
-            foreach (var brand in Brands) {
-                foreach (var hmd in brand.Value) {
-                    foreach (var c in hmd.Value) {
-                        if (c.Value == config) {
+            foreach (var brand in Brands)
+            {
+                foreach (var hmd in brand.Value)
+                {
+                    foreach (var c in hmd.Value)
+                    {
+                        if (c.Value == config)
+                        {
                             path.Add(brand.Key);
                             path.Add(hmd.Key);
                             path.Add(c.Key);
@@ -60,56 +68,75 @@ namespace SCVRPatcher {
             return path;
         }
 
-        public void ToFile(FileInfo file) {
+        public void ToFile(FileInfo file)
+        {
             File.WriteAllText(file.FullName, this.ToJson());//.Replace("\r\n", "\n"););
         }
 
-        public static ConfigDataBase? FromUrl(Uri uri) {
-            try {
-                using (var client = new HttpClient()) {
+        public static ConfigDataBase? FromUrl(Uri uri)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
                     Logger.Info($"Downloading available configs from {uri}...");
                     var response = client.GetAsync(uri).Result;
-                    if (response.IsSuccessStatusCode) {
+                    if (response.IsSuccessStatusCode)
+                    {
                         var json = response.Content.ReadAsStringAsync().Result;
                         return FromJson(json);
-                    } else {
+                    }
+                    else
+                    {
                         throw new Exception($"Failed to download available configs! (Error {response.StatusCode})");
                     }
                 }
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Logger.Error(e);
             }
             return null;
         }
-        public static ConfigDataBase? FromFile(FileInfo file) {
-            if (!file.Exists) {
+        public static ConfigDataBase? FromFile(FileInfo file)
+        {
+            if (!file.Exists)
+            {
                 Logger.Error($"Config file not found: {file.Quote()}");
                 return null;
             }
-            try { File.ReadAllText(file.FullName); } catch (Exception e) {
+            try { File.ReadAllText(file.FullName); }
+            catch (Exception e)
+            {
                 Logger.Error($"Error reading file {file.Quote()}!", e);
             }
             var text = File.ReadAllText(file.FullName);
             Logger.Debug($"Read {text.Length} chars from {file.Quote()}");
             return FromJson(text);
         }
-        public static ConfigDataBase? FromJson(string json) {
-            try {
+        public static ConfigDataBase? FromJson(string json)
+        {
+            try
+            {
                 var db = JsonSerializer.Deserialize<ConfigDataBase>(json, Converter.Settings);
                 db.Raw = json;
                 db.Md5 = json.GetMd5Hash();
                 return db;
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Logger.Error($"Error parsing json: {e} (Length: {json.Length})");
             }
             return null;
         }
     }
-    public static partial class Serialize {
+    public static partial class Serialize
+    {
         public static string ToJson(this ConfigDataBase self) => JsonSerializer.Serialize(self, Converter.Settings);
     }
 
-    public partial class Common {
+    public partial class Common
+    {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("Alternative Interger Resolutions (small list)")]
         public virtual List<Resolution> AlternativeIntergerResolutionsSmallList { get; set; }
@@ -123,7 +150,8 @@ namespace SCVRPatcher {
         public virtual List<Resolution> GiveMeAllTheResolutions { get; set; }
     }
 
-    public partial class Resolution {
+    public partial class Resolution
+    {
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("w")]
         public virtual double? Width { get; set; }
@@ -137,22 +165,26 @@ namespace SCVRPatcher {
         [JsonPropertyName("p")]
         public virtual string? Percentage { get; set; }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             var sb = new StringBuilder($"{Width} x {Height}");
             if (!string.IsNullOrEmpty(Description)) sb.Append($" ({Description})");
             if (!string.IsNullOrEmpty(Percentage)) sb.Append($" {Percentage}%");
             return sb.ToString();
         }
 
-        public static bool operator >(Resolution a, Resolution b) {
+        public static bool operator >(Resolution a, Resolution b)
+        {
             return a.Width > b.Width || a.Height > b.Height;
         }
-        public static bool operator <(Resolution a, Resolution b) {
+        public static bool operator <(Resolution a, Resolution b)
+        {
             return a.Width < b.Width || a.Height < b.Height;
         }
     }
 
-    public partial class HmdConfig {
+    public partial class HmdConfig
+    {
 
         //[JsonIgnore]
         //public string Brand;
@@ -235,8 +267,10 @@ namespace SCVRPatcher {
 
     //public enum VorpXConfigPixel11Zoom { WAwAwAw, YouWillNeedToFindThisSetting };
 
-    internal static class Converter {
-        public static readonly JsonSerializerOptions Settings = new(JsonSerializerDefaults.General) {
+    internal static class Converter
+    {
+        public static readonly JsonSerializerOptions Settings = new(JsonSerializerDefaults.General)
+        {
             Converters =
             {
                 //ErrorReportScFovCap120Converter.Singleton,
@@ -305,15 +339,18 @@ namespace SCVRPatcher {
     //    public static readonly VorpXConfigPixel11ZoomConverter Singleton = new VorpXConfigPixel11ZoomConverter();
     //}
 
-    public class DateOnlyConverter : JsonConverter<DateOnly> {
+    public class DateOnlyConverter : JsonConverter<DateOnly>
+    {
         private readonly string serializationFormat;
         public DateOnlyConverter() : this(null) { }
 
-        public DateOnlyConverter(string? serializationFormat) {
+        public DateOnlyConverter(string? serializationFormat)
+        {
             this.serializationFormat = serializationFormat ?? "yyyy-MM-dd";
         }
 
-        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
             var value = reader.GetString();
             return DateOnly.Parse(value!);
         }
@@ -322,16 +359,19 @@ namespace SCVRPatcher {
             => writer.WriteStringValue(value.ToString(serializationFormat));
     }
 
-    public class TimeOnlyConverter : JsonConverter<TimeOnly> {
+    public class TimeOnlyConverter : JsonConverter<TimeOnly>
+    {
         private readonly string serializationFormat;
 
         public TimeOnlyConverter() : this(null) { }
 
-        public TimeOnlyConverter(string? serializationFormat) {
+        public TimeOnlyConverter(string? serializationFormat)
+        {
             this.serializationFormat = serializationFormat ?? "HH:mm:ss.fff";
         }
 
-        public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
             var value = reader.GetString();
             return TimeOnly.Parse(value!);
         }
@@ -340,7 +380,8 @@ namespace SCVRPatcher {
             => writer.WriteStringValue(value.ToString(serializationFormat));
     }
 
-    internal class IsoDateTimeOffsetConverter : JsonConverter<DateTimeOffset> {
+    internal class IsoDateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+    {
         public override bool CanConvert(Type t) => t == typeof(DateTimeOffset);
 
         private const string DefaultDateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK";
@@ -349,27 +390,32 @@ namespace SCVRPatcher {
         private string? _dateTimeFormat;
         private CultureInfo? _culture;
 
-        public DateTimeStyles DateTimeStyles {
+        public DateTimeStyles DateTimeStyles
+        {
             get => _dateTimeStyles;
             set => _dateTimeStyles = value;
         }
 
-        public string? DateTimeFormat {
+        public string? DateTimeFormat
+        {
             get => _dateTimeFormat ?? string.Empty;
             set => _dateTimeFormat = (string.IsNullOrEmpty(value)) ? null : value;
         }
 
-        public CultureInfo Culture {
+        public CultureInfo Culture
+        {
             get => _culture ?? CultureInfo.CurrentCulture;
             set => _culture = value;
         }
 
-        public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options) {
+        public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+        {
             string text;
 
 
             if ((_dateTimeStyles & DateTimeStyles.AdjustToUniversal) == DateTimeStyles.AdjustToUniversal
-                || (_dateTimeStyles & DateTimeStyles.AssumeUniversal) == DateTimeStyles.AssumeUniversal) {
+                || (_dateTimeStyles & DateTimeStyles.AssumeUniversal) == DateTimeStyles.AssumeUniversal)
+            {
                 value = value.ToUniversalTime();
             }
 
@@ -378,16 +424,23 @@ namespace SCVRPatcher {
             writer.WriteStringValue(text);
         }
 
-        public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
             string? dateText = reader.GetString();
 
-            if (string.IsNullOrEmpty(dateText) == false) {
-                if (!string.IsNullOrEmpty(_dateTimeFormat)) {
+            if (string.IsNullOrEmpty(dateText) == false)
+            {
+                if (!string.IsNullOrEmpty(_dateTimeFormat))
+                {
                     return DateTimeOffset.ParseExact(dateText, _dateTimeFormat, Culture, _dateTimeStyles);
-                } else {
+                }
+                else
+                {
                     return DateTimeOffset.Parse(dateText, Culture, _dateTimeStyles);
                 }
-            } else {
+            }
+            else
+            {
                 return default(DateTimeOffset);
             }
         }
